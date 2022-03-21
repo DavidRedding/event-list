25f8383117961864caeaacdf86de33e29c27934d
David Redding <redding.david35@gmail.com>
2022-03-21 16:00:51 -0500

    event object mapping & listeners. + delete feature

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
-const App = () => <div>24</div>;
+import { useState } from 'react';
+
+const App = () => {
+  // Data
+  const [events, setEvents] = useState([
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: 'race on moo moo farm', id: 3 },
+  ]);
+
+  // Listener
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+  };
+
+  // Map
+  const list = events.map((event) => (
+    <div className="text-center py-5" key={event.id}>
+      <h2 className=" text-xl font-bold mb-2">{event.title}</h2>
+      <button className=" bg-green-300 p-1 px-4" onClick={() => handleClick(event.id)}>
+        delete event
+      </button>
+    </div>
+  ));
+
+  // Return
+  return <div className=" min-h-screen ">{list}</div>;
+};
 
 export default App;
