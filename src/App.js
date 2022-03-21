ff1641d3e021bc2b8bb13b16ed0dfc8ed126c1f4
David Redding <redding.david35@gmail.com>
2022-03-21 16:48:03 -0500

    show/hide button + functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react';
 
 const App = () => {
   // Data
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser's live stream", id: 2 },
     { title: 'race on moo moo farm', id: 3 },
   ]);
 
-  // Listener
+  // Listener CallBack
   const handleClick = (id) => {
     setEvents((prevEvents) => {
       return prevEvents.filter((event) => {
@@ -19,16 +20,25 @@ const App = () => {
 
   // Map
   const list = events.map((event) => (
-    <div className="text-center py-5" key={event.id}>
+    <div className="py-5 flex flex-col items-center" key={event.id}>
       <h2 className=" text-xl font-bold mb-2">{event.title}</h2>
-      <button className=" bg-green-300 p-1 px-4" onClick={() => handleClick(event.id)}>
+      <button className=" bg-yellow-300 p-1 w-1/2" onClick={() => handleClick(event.id)}>
         delete event
       </button>
     </div>
   ));
 
   // Return
-  return <div className=" min-h-screen ">{list}</div>;
+  return (
+    <div className=" min-h-screen bg-yellow-500">
+      <div className=" flex justify-center">
+        <button onClick={() => setShowEvents(!showEvents)} className=" bg-yellow-100 p-1 w-1/3 ">
+          {showEvents ? 'hide events' : 'show events'}
+        </button>
+      </div>
+      {showEvents ? list : null}
+    </div>
+  );
 };
 
 export default App;
