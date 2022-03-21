76b6b9e24d80064bb180fbb15b1cbfeeb2971438
David Redding <redding.david35@gmail.com>
2022-03-21 17:47:49 -0500

    portal implementation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,15 @@
-const Modal = ({ children, showModal, setShowModal }) => (
-  <div className=" fixed top-0 left-0 w-full h-full bg-black/25">
-    <div className=" py-10 max-w-md mt-52 mx-auto bg-yellow-50 rounded-lg text-center">
-      {children}
-      <button className="mt-4 bg-yellow-200 px-7 py-1 rounded" onClick={() => setShowModal(!showModal)}>
-        close
-      </button>
-    </div>
-  </div>
-);
+import ReactDOM from 'react-dom';
+const Modal = ({ children, showModal, setShowModal }) =>
+  ReactDOM.createPortal(
+    <div className=" fixed top-0 left-0 w-full h-full bg-black/25">
+      <div className=" py-10 max-w-md mt-52 mx-auto bg-yellow-50 rounded-lg text-center">
+        {children}
+        <button className="mt-4 bg-yellow-200 px-7 py-1 rounded" onClick={() => setShowModal(!showModal)}>
+          close
+        </button>
+      </div>
+    </div>,
+    document.body
+  );
 
 export default Modal;
