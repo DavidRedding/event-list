8002747963c201d3f4cfc9a06e62d8ca815f9f62
David Redding <redding.david35@gmail.com>
2022-03-22 11:19:59 -0500

    ability to add & display location via selectbox

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,7 +2,10 @@ const EventList = ({ events, handleClick }) => {
   // Map
   const list = events.map((event) => (
     <div className="py-5 flex flex-col items-center" key={event.id}>
-      <h2 className=" text-xl font-bold mb-2 text-yellow-200">{event.title}</h2>
+      <h2 className=" text-xl font-bold  text-yellow-200">{event.title}</h2>
+      <p className="mb-2 text-yellow-200">
+        {event.location} - {event.date}
+      </p>
       <button className=" bg-yellow-300 p-1 w-1/2 text-yellow-700 rounded" onClick={() => handleClick(event.id)}>
         delete event
       </button>
--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -3,26 +3,31 @@ import { useState } from 'react';
 const NewEventForm = ({ addEvent }) => {
   const [title, setTitle] = useState('');
   const [date, setDate] = useState('');
+  const [location, setLocation] = useState('manchester');
+
+  const reset = () => {
+    setDate('');
+    setTitle('');
+    setLocation('manchester');
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const event = {
       title: title,
       date: date,
+      location: location,
       id: Math.floor(Math.random() * 1000),
     };
     console.log(event);
     addEvent(event);
     reset();
   };
-  const reset = () => {
-    setDate('');
-    setTitle('');
-  };
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col space-y-6 max-w-sm bg-rose-5 mx-auto">
-      <label className=" flex flex-col items-start ">
+      <label className=" text-left ">
         <span>Event Title:</span>
         <input
           type="text"
@@ -31,7 +36,7 @@ const NewEventForm = ({ addEvent }) => {
           value={title}
         />
       </label>
-      <label className=" flex flex-col items-start">
+      <label className=" text-left">
         <span>Event Date:</span>
         <input
           type="date"
@@ -40,7 +45,15 @@ const NewEventForm = ({ addEvent }) => {
           value={date}
         />
       </label>
-      <button className="  w-1/3 mx-auto  bg-yellow-300 hover:bg-yellow-200 px-7 py-1 rounded">Submit</button>
+      <label className="text-left">
+        <span>Event Location: </span>
+        <select className=" border outline-none focus:border-yellow-400" onChange={(e) => setLocation(e.target.value)}>
+          <option value="manchester">Manchester</option>
+          <option value="london">London</option>
+          <option value="cardiff">Cardiff</option>
+        </select>
+      </label>
+      <button className="  w-1/3 mx-auto bg-yellow-300 hover:bg-yellow-200 px-7 py-1 rounded">Submit</button>
     </form>
   );
 };
