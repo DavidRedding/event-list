8f292439e8e66e1c9b887925e375ad3cfee5601b
David Redding <redding.david35@gmail.com>
2022-03-22 10:50:16 -0500

    implemented useRef instead of useState

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,44 +1,38 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const NewEventForm = ({ addEvent }) => {
-  const [title, setTitle] = useState('');
-  const [date, setDate] = useState('');
+  const title = useRef();
+  const date = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(title, date);
+
     const event = {
-      title: title,
-      date: date,
+      title: title.current.value,
+      date: date.current.value,
       id: Math.floor(Math.random() * 1000),
     };
+
     console.log(event);
     addEvent(event);
     reset();
   };
+
   const reset = () => {
-    setDate('');
-    setTitle('');
+    title.current.value = '';
+    date.current.value = '';
   };
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col space-y-6 max-w-sm bg-rose-5 mx-auto">
       <label className=" flex flex-col items-start ">
         <span>Event Title:</span>
-        <input
-          type="text"
-          onChange={(e) => setTitle(e.target.value)}
-          className=" p-1 rounded w-full outline-none border focus:border-yellow-300"
-          value={title}
-        />
+        <input ref={title} type="text" className=" p-1 rounded w-full outline-none border focus:border-yellow-300" />
       </label>
       <label className=" flex flex-col items-start">
         <span>Event Date:</span>
-        <input
-          type="date"
-          onChange={(e) => setDate(e.target.value)}
-          className="p-1 rounded w-full outline-none border focus:border-yellow-300"
-          value={date}
-        />
+        <input ref={date} type="date" className="p-1 rounded w-full outline-none border focus:border-yellow-300" />
       </label>
       <button className="  w-1/3 mx-auto  bg-yellow-300 hover:bg-yellow-200 px-7 py-1 rounded">Submit</button>
     </form>
